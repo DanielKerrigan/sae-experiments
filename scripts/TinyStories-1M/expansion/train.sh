ff00f36a0e945b4c495a86250d2ad60e93af702f
DanielKerrigan <d.kerrigan111@gmail.com>
2024-08-02 09:05:45 -0400

    vary expansion factor

---
+++ scripts/TinyStories-1M/expansion/train.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --partition=gpu
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:v100-sxm2:1
+#SBATCH --job-name=ts-1m
+#SBATCH --mem=10GB
+#SBATCH --ntasks=1
+#SBATCH --output=/scratch/kerrigan.d/mi/sae-experiments/output/TinyStories-1M_expansion_train_%A_%a.out
+#SBATCH --error=/scratch/kerrigan.d/mi/sae-experiments/output/TinyStories-1M_expansion_train_%A_%a.err
+#SBATCH --array=4,8,16,32
+
+source /home/kerrigan.d/miniconda3/bin/activate
+conda activate sae-experiments
+
+srun python train.py -e $SLURM_ARRAY_TASK_ID --path /scratch/kerrigan.d/mi/sae-experiments
